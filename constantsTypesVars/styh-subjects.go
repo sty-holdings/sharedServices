81b3d85597514cea40227f5bb0b4fce7f4fc9cc9
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2024-11-15 17:17:38 -0800

    Split subjects from sty-holdings.go

--- constantsTypesVars/styh-subjects.go
+++ constantsTypesVars/styh-subjects.go
@@ -2,12 +2,6 @@ package sharedServices
 
 //goland:noinspection All
 const (
-	// Text strings
-	TXT_AI2C_KEY = "AI2C KEY: "
-)
-
-//goland:noinspection All
-const (
 	// Subjects
 	NCI_PING           = "ping"
 	NCI_TURN_DEBUG_OFF = "turn-debug-off"
@@ -41,72 +35,4 @@ const (
 	SUB_SYNADIA_LIST_SYSTEM_SERVER_INFO     = "synadia.list-system-server-info"
 	SUB_SYNADIA_LIST_TEAMS                  = "synadia.list-teams"
 	SUB_SYNADIA_LIST_TEAM_SERVER_ACCOUNTS   = "synadia.list-team-server-accounts"
-)
-
-//goland:noinspection All
-const (
-	// Extensions
-	EXTENSION_GEMINI     = "gemini"
-	EXTENSION_HTTPS_NATS = "https-nats"
-	NC_INTERNAL          = "nc-internal"
-	EXTENSION_STRIPE     = "stripe"
-	EXTENSION_SENDGRID   = "sendgrid"
-	EXTENSION_SYNADIA    = "synadia"
-)
-
-//goland:noinspection All
-const (
-	// Fully qualified filenames
-	FQN_CERTIFICATE      = "CertificateFQN"
-	FQN_GCP_CREDENTIALS  = "GCPCredentialsFQN"
-	FQN_NATS_CREDENTIALS = "NATSCredsFQN"
-	FQN_TLS_CABUNDLE     = "TLS:TLSCABundle"
-	FQN_TLS_CERTIFICATE  = "TLS:TLSCert"
-	FQN_TLS_PRIVATE_KEY  = "TLS:TLSKey"
-)
-
-//goland:noinspection All
-const (
-	// Testing
-	TEST_POSITIVE_SUCCESS = "Positive Case: Successful: "
-	TEST_NEGATIVE_SUCCESS = "Negative Case: Successful: "
-)
-
-//goland:noinspection All
-const (
-	// Software Values
-	ATTEMPT_LIMIT_EXCEEDED  = "Attempt limit exceeded"
-	AUTHENTICATOR_SERVICE   = "AuthenticatorService"
-	DEFAULT_VERSION         = "9999.9999.9999"
-	ENVIRONMENT_LOCAL       = "local"
-	ENVIRONMENT_DEVELOPMENT = "development"
-	ENVIRONMENT_DEMO        = "demo"
-	ENVIRONMENT_PRODUCTION  = "production"
-	IDP_FIREBASE            = "firebase"
-	IDP_COGNITO             = "cognito"
-	LOCAL_HOST              = "localhost"
-	NATS_NON_TLS_CONNECTION = "NON-TLS"
-	NATS_TLS_CONNECTION     = "TLS"
-	OPER_DOUBE_EQUAL_SIGN   = "=="
-	OPER_EQUAL_SIGN         = "="
-	PID_FILENAME            = "server.pid"
-	VAL_EMPTY               = ""
-	VAL_ZERO                = 0
-	VALID                   = "VALID"
-)
-
-//goland:noinspection All
-const (
-	// Special values used to trigger requests
-	PAYMENT_METHOD_LIST = "list"
-)
-
-//goland:noinspection All
-const (
-	DATASTORE_USERS                = "users"
-	DATASTORE_QUESTIONS_ANSWERS    = "questions_answers"
-	DATASTORE_SOURCE_DATA          = "source_data"
-	DATASTORE_USERS_SAAS_PROVIDERS = "users_saas_providers"
-	DATASTORE_TRAINING_QUESTIONS   = "training_questions"
-	DATASTORE_QUESTION_ANALYSIS    = "question_analysis"
 )
