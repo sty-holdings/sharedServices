352c2812250b1d15a13016725c1a312246d28fe3
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2024-11-29 16:57:00 -0800

    Split extensions out of sty-holdings.go and added Brain extesion

--- constantsTypesVars/sty-holdings.go
+++ constantsTypesVars/sty-holdings.go
@@ -8,18 +8,6 @@ const (
 
 //goland:noinspection All
 const (
-	// Extensions
-	EXTENSION_ANALYZE_QUESTION = "analyze-question"
-	EXTENSION_HTTPS_NATS       = "https-nats"
-	NC_INTERNAL                = "nc-internal"
-	EXTENSION_SAAS_PROFILE     = "saas-profile"
-	EXTENSION_SENDGRID         = "sendgrid"
-	EXTENSION_STRIPE           = "stripe"
-	EXTENSION_SYNADIA          = "synadia"
-)
-
-//goland:noinspection All
-const (
 	// Fully qualified filenames
 	FQN_CERTIFICATE      = "CertificateFQN"
 	FQN_GCP_CREDENTIALS  = "GCPCredentialsFQN"
