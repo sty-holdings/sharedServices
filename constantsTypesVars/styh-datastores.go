4360fadd3ad4213cd6e3bb735886ce8bb2a5a001
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2024-12-09 15:24:12 -0800

    Changed DATASTORE_CATEGORY_SAAS_PROVIDER_MAP to DATASTORE_SUPPORT_CATEGORY_SAAS_PROVIDER

--- constantsTypesVars/styh-datastores.go
+++ constantsTypesVars/styh-datastores.go
@@ -2,10 +2,10 @@ package sharedServices
 
 //goland:noinspection All
 const (
-	DATASTORE_ANALYZED_QUESTIONS         = "analyzed_questions"
-	DATASTORE_ANSWER_SOURCE_DATA         = "answer_source_data"
-	DATASTORE_CATEGORY_SAAS_PROVIDER_MAP = "category_saas_provider_map"
-	DATASTORE_QUESTIONS_ANSWERS          = "questions_answers"
-	DATASTORE_TRAINING_QUESTIONS         = "training_questions"
-	DATASTORE_USERS                      = "users"
+	DATASTORE_ANALYZED_QUESTIONS             = "analyzed_questions"
+	DATASTORE_ANSWER_SOURCE_DATA             = "answer_source_data"
+	DATASTORE_SUPPORT_CATEGORY_SAAS_PROVIDER = "support_category_saas_provider"
+	DATASTORE_QUESTIONS_ANSWERS              = "questions_answers"
+	DATASTORE_TRAINING_QUESTIONS             = "training_questions"
+	DATASTORE_USERS                          = "users"
 )
