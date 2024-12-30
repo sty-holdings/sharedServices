f6dfd3d25904abdeb309b540f6eafd3befc429f5
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2024-12-30 11:58:55 -0800

    Removed extra newline

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -5,5 +5,5 @@ const (
 	// Categories
 	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request. \n " +
 		"\u2022 Has your information been updated in your service provider. \n" +
-		"\n \u2022 Try a broader search: Use more general keywords related to the SAAS provider."
+		"\u2022 Try a broader search: Use more general keywords related to the SAAS provider."
 )
