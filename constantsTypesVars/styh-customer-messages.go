8d3379ff72fa5884068adb402291dbb8d73d3f9d
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2024-12-30 12:00:51 -0800

    Removed unneeded space.

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,7 +3,7 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request. \n " +
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request. \n" +
 		"\u2022 Has your information been updated in your service provider. \n" +
 		"\u2022 Try a broader search: Use more general keywords related to the SAAS provider."
 )
