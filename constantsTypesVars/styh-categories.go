d8742a40e0e226477b8c884f5f1acc911b39746c
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2024-12-30 17:01:10 -0800

    Updated sales categores, removed un-used categories, and added new labels

--- constantsTypesVars/styh-categories.go
+++ constantsTypesVars/styh-categories.go
@@ -2,122 +2,13 @@ package sharedServices
 
 //goland:noinspection All
 const (
-	// Categories
-	CATEGORY_CUSTOMER_SERVICE       = "customer_service"
-	CATEGORY_FINANCE                = "finance"
-	CATEGORY_HUMAN_RESOURCES        = "human_resources"
-	CATEGORY_INFORMATION_TECHNOLOGY = "information_technology"
-	CATEGORY_MARKETING              = "marketing"
-	CATEGORY_OPERATIONS             = "operations"
-	CATEGORY_RESEARCH_DEVELOPMENT   = "research_development"
-	CATEGORY_SALES                  = "sales"
-	CATEGORY_WEATHER                = "weather"
-	CATEGORY_DATE                   = "date"
-)
-
-//goland:noinspection All
-const (
-	CUSTOMER_SERVICE_SUB_CUSTOMER_SUPPORT_ASSISTANCE    = "customer_support_assistance"
-	CUSTOMER_SERVICE_SUB_COMPLAINT_RESOLUTION           = "complaint_resolution"
-	CUSTOMER_SERVICE_SUB_CUSTOMER_SATISFACTION_FEEDBACK = "customer_satisfaction_feedback"
-	CUSTOMER_SERVICE_SUB_CALL_CENTER_OPERATIONS         = "call_center_operations"
-	CUSTOMER_SERVICE_SUB_SOCIAL_MEDIA_SUPPORT           = "social_media_support"
-	CUSTOMER_SERVICE_SUB_TECHNICAL_SUPPORT              = "technical_support"
-	CUSTOMER_SERVICE_SUB_CUSTOMER_LOYALTY_PROGRAMS      = "customer_loyalty_programs"
-	CUSTOMER_SERVICE_SUB_WEBSITE_SUPPORT                = "website_support"
-)
-
-//goland:noinspection All
-const (
-	FINANCE_SUB_ACCOUNTING            = "accounting"
-	FINANCE_SUB_FINANCIAL_PLANNING    = "financial_planning"
-	FINANCE_SUB_FINANCIAL_ANALYSIS    = "financial_analysis"
-	FINANCE_SUB_TREASURY              = "treasury"
-	FINANCE_SUB_CASH_FLOW             = "cash_flow"
-	FINANCE_SUB_TAX                   = "tax"
-	FINANCE_SUB_INVESTOR_RELATIONS    = "investor_relations"
-	FINANCE_SUB_AUDIT_INTERNAL        = "audit_internal"
-	FINANCE_SUB_AUDIT_EXTERNAL        = "audit_external"
-	FINANCE_SUB_ACCOUNTS_PAYABLE      = "accounts_payable"
-	FINANCE_SUB_ACCOUNTS_RECEIVABLE   = "accounts_receivable"
-	FINANCE_SUB_PAYROLL               = "payroll"
-	FINANCE_SUB_BUDGETING_FORECASTING = "budgeting_forecasting"
-	FINANCE_SUB_FINANCIAL_REPORTING   = "financial_reporting"
-	FINANCE_SUB_COST_ACCOUNTING       = "cost_accounting"
-	FINANCE_SUB_RISK                  = "risk"
-	FINANCE_SUB_INVESTMENT_ANALYSIS   = "investment_analysis"
-	FINANCE_SUB_MERGERS               = "merger"
-	FINANCE_SUB_ACQUISITIONS          = "acquisitions"
-)
-
-//goland:noinspection All
-const (
-	HUMAN_RESOURCES_SUB_TALENT_ACQUISITION_        = "talent_acquisition"
-	HUMAN_RESOURCES_SUB_EMPLOYEE_ONBOARDING        = "employee_onboarding"
-	HUMAN_RESOURCES_SUB_EMPLOYEE_TRAINING          = "employee_training"
-	HUMAN_RESOURCES_SUB_COMPENSATION               = "compensation"
-	HUMAN_RESOURCES_SUB_BENEFITS                   = "benefits"
-	HUMAN_RESOURCES_SUB_EMPLOYEE_RELATIONS         = "employee_relations"
-	HUMAN_RESOURCES_SUB_EMPLOYEE_PERFORMANCE       = "employee_performance"
-	HUMAN_RESOURCES_SUB_DIVERSITY_INCLUSION        = "diversity_inclusion"
-	HUMAN_RESOURCES_SUB_TALENT_DEVELOPMENT         = "talent_development"
-	HUMAN_RESOURCES_SUB_SUCCESSION_PLANNING        = "succession_planning"
-	HUMAN_RESOURCES_SUB_HEALTH                     = "health"
-	HUMAN_RESOURCES_SUB_SAFETY                     = "safety"
-	HUMAN_RESOURCES_SUB_LABOR_RELATIONS            = "labor_relations"
-	HUMAN_RESOURCES_SUB_ORGANIZATIONAL_DEVELOPMENT = "organizational_development"
-)
-
-//goland:noinspection All
-const (
-	MARKETING_SUB_MARKET_RESEARCH                       = "market_research"
-	MARKETING_SUB_BRANDING                              = "branding"
-	MARKETING_SUB_ADVERTISING                           = "advertising"
-	MARKETING_SUB_DIGITAL_MARKETING                     = "digital_marketing"
-	MARKETING_SUB_PUBLIC_RELATIONS                      = "public_relations"
-	MARKETING_SUB_BUSINESS_DEVELOPMENTCONTENT_MARKETING = "business_developmentcontent_marketing"
-	MARKETING_SUB_SEARCH_ENGINE_OPTIMIZATION            = "search_engine_optimization"
-	MARKETING_SUB_SOCIAL_MEDIA_MARKETING                = "social_media_marketing"
-	MARKETING_SUB_EMAIL_MARKETING                       = "email_marketing"
-	MARKETING_SUB_PAY_PER_CLICK_ADVERTISING             = "pay_per_click_advertising"
-	MARKETING_SUB_PRODUCT_MARKETING                     = "product_marketing"
-	MARKETING_SUB_CUSTOMER_RELATIONSHIP                 = "customer_relationship"
-	MARKETING_SUB_EVENT_MARKETING                       = "event_marketing"
-)
-
-//goland:noinspection All
-const (
-	OPERATIONOS_SUB_PRODUCTION             = "production"
-	OPERATIONOS_SUB_SUPPLY_CHAIN           = "supply_chain"
-	OPERATIONOS_SUB_LOGISTICS_DISTRIBUTION = "logistics_distribution"
-	OPERATIONOS_SUB_QUALITY_ASSURANCE      = "quality_assurance"
-	OPERATIONOS_SUB_INVENTORY              = "inventory"
-	OPERATIONOS_SUB_PROCUREMENT            = "procurement"
-	OPERATIONOS_SUB_DEMAND_PLANNING        = "demand_planning"
-	OPERATIONOS_SUB_PROCESS_IMPROVEMENT    = "process_improvement"
-	OPERATIONOS_SUB_FACILITY_MANAGEMENT    = "facility_management"
-)
-
-//goland:noinspection All
-const (
-	SALES_SUB_SALES_STRATEGY       = "sales_strategy"
-	SALES_SUB_SALES_PLANNING       = "sales_planning"
-	SALES_SUB_SALES_OPERATIONS     = "sales_operations"
-	SALES_SUB_CUSTOMER_ACQUISITION = "customer_acquisition"
-	SALES_SUB_CUSTOMER_RETENTION   = "customer_retention"
-	SALES_SUB_INSIDE_SALES         = "inside_sales"
-	SALES_SUB_OUTSIDE_SALES        = "outside_sales"
-	SALES_SUB_ACCOUNT_MANAGEMENT   = "account_management"
-	SALES_SUB_LEAD_GENERATION      = "lead_generation"
-	SALES_SUB_SALES_TRAINING       = "sales_training"
-)
-
-//goland:noinspection All
-const (
-	RESEARCH_DEVELOPER_SUB_PRODUCT_DEVELOPMENT_INNOVATION = "product_development_innovation"
-	RESEARCH_DEVELOPER_SUB_TECHNOLOGY_RESEARCH            = "technology_research"
-	RESEARCH_DEVELOPER_SUB_MARKET_RESEARCH_ANALYSIS       = "market_research_analysis"
-	RESEARCH_DEVELOPER_SUB_PROTOTYPING                    = "prototyping"
-	RESEARCH_DEVELOPER_SUB_TESTING_VALIDATION             = "testing_validation"
-	RESEARCH_DEVELOPER_SUB_PATENT_INTELLECTUAL_PROPERTY   = "patent_intellectual_property"
+	SALES_SUB_SALES_STRATEGY           = "sales-strategy"
+	SALES_SUB_SALES_PLANNING           = "sales-planning"
+	SALES_SUB_SALES_OPERATIONS         = "sales-operations"
+	SALES_SUB_SALES_ACQUISITION        = "sakes-acquisition"
+	SALES_SUB_SALES_RETENTION          = "sales-retention"
+	SALES_SUB_SALES_INSIDE             = "sales-inside"
+	SALES_SUB_SALES_ACCOUNT_MANAGEMENT = "sales-account-management"
+	SALES_SUB_SALES_LEAD_GENERATION    = "sales-lead-generation"
+	SALES_SUB_SALES_TRAINING           = "sales-training"
 )
