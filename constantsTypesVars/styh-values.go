a40278aa92b47995a5f8cb1b92af583336a042b9
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-01-05 16:02:43 -0800

    Added VAL_NO_CHARGE_DATA

--- constantsTypesVars/styh-values.go
+++ constantsTypesVars/styh-values.go
@@ -2,8 +2,9 @@ package sharedServices
 
 //goland:noinspection All
 const (
-	VAL_EMPTY         = ""
-	VAL_ZERO          = 0
-	VAL_VALID         = "VALID"
-	VAL_NOT_SUPPORTED = "not supported"
+	VAL_EMPTY          = ""
+	VAL_NO_CHARGE_DATA = "{\"dk_charges\":null}"
+	VAL_NOT_SUPPORTED  = "not supported"
+	VAL_VALID          = "VALID"
+	VAL_ZERO           = 0
 )
