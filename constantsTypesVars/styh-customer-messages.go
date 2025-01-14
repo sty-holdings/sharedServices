c9155f7d827ca1af7eeadcfa69e1787eec01bf0f
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-01-14 14:13:37 -0800

    Refactored to runes and added CUST_MSG_NEED_DATE_FORMATTED

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,7 +3,11 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request. \n" +
-		"\u2022 Has your information been updated in your service provider. \n" +
-		"\u2022 Try a broader search: Use more general keywords related to the SAAS provider."
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = `Hmm, it looks like we couldn't find any information matching your request.
+		\u2022 Has your information been updated in your service provider.
+		\u2022 Try a broader search: Use more general keywords related to the SAAS provider.`
+	CUST_MSG_NEED_DATE_FORMATTED = `The question doesn't have a timeframe'.
+		\u2022 Added a date.
+		\u2022 Added words like current, previous, today, yesterday
+		\u2022 Added last and the timeframe. ex: last week or last month`
 )
