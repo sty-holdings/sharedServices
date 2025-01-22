7550f45e712d86b84be97fd39604ec8cee9ec4d5
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-01-22 12:47:27 -0800

    Added line to CUST_MSG_NEED_DATE_FORMATTED

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -8,6 +8,7 @@ const (
 		\u2022 Try a broader search: Use more general keywords related to the SAAS provider.`
 	CUST_MSG_NEED_DATE_FORMATTED = `The question doesn't have a timeframe'.
 		\u2022 Added a date.
+        \u2022 Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions.
 		\u2022 Added words like current, previous, today, yesterday
 		\u2022 Added last and the timeframe. ex: last week or last month`
 )
