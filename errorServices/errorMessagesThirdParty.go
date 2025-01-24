a2dce0d9617b66a3332239dce6abf3425c57afc4
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-01-24 15:41:43 -0800

    Added FIREBASE_AUTH_USER_NOT_FOUND to third party errors

--- errorServices/errorMessagesThirdParty.go
+++ errorServices/errorMessagesThirdParty.go
@@ -9,6 +9,7 @@ const (
 	UNKNOWN                       = "UNKNOWN"
 	JWT_TOKEN_SIGNATURE_INVALID   = "token signature is invalid: crypto/rsa: verification error"
 	FIREBASE_AUTH_BAD_CREDENTIALS = "cannot read credentials"
+	FIREBASE_AUTH_USER_NOT_FOUND  = "cannot find user from uid:"
 	NATS_NOT_CONNECTED            = "nats: message is not bound to subscription/connection"
 	NATS_INVALID_CONNECTION       = "nats: invalid connection"
 )
