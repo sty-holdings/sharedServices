fbc12f6c125c69d379634307592f7f704317b813
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-01-25 11:24:02 -0800

    Added gRPC labels, test harness, and service

---
+++ grpcServices/grpcServices.go
@@ -0,0 +1,111 @@
+package sharedServices
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	ctv "github.com/sty-holdings/sharedServices/v2025/constantsTypesVars"
+	errs "github.com/sty-holdings/sharedServices/v2025/errorServices"
+	hlps "github.com/sty-holdings/sharedServices/v2025/helpers"
+)
+
+// NewGRPCService - builds a reusable gRPC Service that creates an instance name and builds a connection. The GRPCPort must be at or above 50051.
+//
+//	Customer Messages: None
+//	Errors: None
+//	Verifications: None
+func NewGRPCService(
+	extensionName string,
+	config GRPCConfiguration,
+) (gRPCServicePtr *GRPCService, errorInfo errs.ErrorInfo) {
+
+	var (
+		tCACertificateFile    []byte
+		tCertificate          tls.Certificate
+		tCACertPool           *x509.CertPool
+		tGRPCListener         net.Listener
+		tTLSConfig            *tls.Config
+		tTransportCredentials credentials.TransportCredentials
+	)
+
+	if errorInfo = hlps.CheckValueNotEmpty(extensionName, errs.ErrRequiredParameterMissing, ctv.LBL_EXTENSION_NAME); errorInfo.Error != nil {
+		return
+	}
+	if errorInfo = hlps.CheckValueNotEmpty(config.GRPCHost, errs.ErrRequiredParameterMissing, ctv.LBL_HOST); errorInfo.Error != nil {
+		return
+	}
+	if config.GRPCPort < ctv.VAL_GRPC_MIN_PORT {
+		errorInfo = errs.NewErrorInfo(errs.ErrGRPCPortInvalud, errs.BuildLabelValue(ctv.LBL_GRPC_PORT, strconv.Itoa(config.GRPCPort)))
+		return
+	}
+	if errorInfo = hlps.CheckValueNotEmpty(config.GRPCTLSInfo.TLSCABundleFQN, errs.ErrRequiredParameterMissing, ctv.LBL_TLS_CA_BUNDLE_FILENAME); errorInfo.Error != nil {
+		return
+	}
+	if errorInfo = hlps.CheckValueNotEmpty(config.GRPCTLSInfo.TLSCertFQN, errs.ErrRequiredParameterMissing, ctv.LBL_TLS_CERTIFICATE_FILENAME); errorInfo.Error != nil {
+		return
+	}
+	if errorInfo = hlps.CheckValueNotEmpty(config.GRPCTLSInfo.TLSPrivateKeyFQN, errs.ErrRequiredParameterMissing, ctv.LBL_TLS_PRIVATE_KEY_FILENAME); errorInfo.Error != nil {
+		return
+	}
+	if errorInfo = hlps.CheckValueNotEmpty(config.GRPCTLSInfo.TLSCABundleFQN, errs.ErrRequiredParameterMissing, ctv.LBL_TLS_CA_BUNDLE_FILENAME); errorInfo.Error != nil {
+		return
+	}
+
+	gRPCServicePtr = &GRPCService{
+		host:     config.GRPCHost,
+		secure:   config.GRPCSecure,
+		userInfo: ctv.UserInfo{},
+	}
+
+	if tGRPCListener, errorInfo.Error = net.Listen(ctv.VAL_TCP, fmt.Sprintf("%s:%d", config.GRPCHost, config.GRPCPort)); errorInfo.Error != nil {
+		errorInfo = errs.NewErrorInfo(errorInfo.Error, errs.BuildLabelValue(ctv.LBL_GRPC_LISTENER, ctv.TXT_FAILED))
+		return
+	}
+	gRPCServicePtr.gRPCListenerPtr = &tGRPCListener
+
+	if tCertificate, errorInfo.Error = tls.LoadX509KeyPair(config.GRPCTLSInfo.TLSCertFQN, config.GRPCTLSInfo.TLSPrivateKeyFQN); errorInfo.Error != nil {
+		errorInfo = errs.NewErrorInfo(
+			errorInfo.Error, fmt.Sprintf(
+				"%s, %s", errs.BuildLabelValue(ctv.LBL_TLS_CERTIFICATE_FILENAME, config.GRPCTLSInfo.TLSCertFQN), errs.BuildLabelValue(
+					ctv.LBL_TLS_PRIVATE_KEY_FILENAME,
+					config.GRPCTLSInfo.TLSPrivateKeyFQN,
+				),
+			),
+		)
+		return
+	}
+	if tCACertificateFile, errorInfo.Error = os.ReadFile(config.GRPCTLSInfo.TLSCABundleFQN); errorInfo.Error != nil {
+		errorInfo = errs.NewErrorInfo(errorInfo.Error, errs.BuildLabelValue(ctv.LBL_TLS_CA_BUNDLE_FILENAME, config.GRPCTLSInfo.TLSCABundleFQN))
+		return
+	}
+	tCACertPool = x509.NewCertPool()
+	if ok := tCACertPool.AppendCertsFromPEM(tCACertificateFile); !ok {
+		errorInfo = errs.NewErrorInfo(errorInfo.Error, errs.BuildLabelValue(ctv.LBL_TLS_CA_CERT_POOL, ctv.TXT_FAILED))
+		return
+	}
+	tTransportCredentials = credentials.NewTLS(tTLSConfig)
+
+	tTLSConfig = &tls.Config{
+		Certificates: []tls.Certificate{tCertificate},
+		ClientCAs:    tCACertPool,
+	}
+
+	if config.GRPCSecure {
+		tTLSConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	} else {
+		tTLSConfig.ClientAuth = tls.NoClientCert
+	}
+
+	gRPCServicePtr.gRPCServerPtr = grpc.NewServer(grpc.Creds(tTransportCredentials))
+
+	return
+}
+
+//  Private Functions
---
+++ grpcServices/grpcServices_test.go
@@ -0,0 +1,71 @@
+package sharedServices
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"testing"
+
+	ctv "github.com/sty-holdings/sharedServices/v2025/constantsTypesVars"
+	errs "github.com/sty-holdings/sharedServices/v2025/errorServices"
+	jwts "github.com/sty-holdings/sharedServices/v2025/jwtServices"
+)
+
+func TestGetNATSConnection(tPtr *testing.T) {
+
+	type arguments struct {
+		config GRPCConfiguration
+	}
+
+	var (
+		errorInfo          errs.ErrorInfo
+		gotError           bool
+		tFunction, _, _, _ = runtime.Caller(0)
+		tFunctionName      = runtime.FuncForPC(tFunction).Name()
+	)
+
+	tests := []struct {
+		name      string
+		arguments arguments
+		wantError bool
+	}{
+		{
+			name: ctv.TEST_POSITIVE_SUCCESS + "Secure connection.",
+			arguments: arguments{
+				config: GRPCConfiguration{
+					GRPCHost:   "localhost",
+					GRPCPort:   50051,
+					GRPCSecure: false,
+					GRPCTLSInfo: jwts.TLSInfo{
+						TLSCert:          "",
+						TLSCertFQN:       "/Volumes/development-share/.keys/sty-holdings.net/local/local_sty-holdings_net/local_sty-holdings_net.crt",
+						TLSPrivateKey:    "",
+						TLSPrivateKeyFQN: "/Volumes/development-share/.keys/sty-holdings.net/local/local_sty-holdings_net/local-sty-holdings-net-private.key",
+						TLSCABundle:      "",
+						TLSCABundleFQN:   "/Volumes/development-share/.keys/sty-holdings.net/local/local_sty-holdings_net/local_sty-holdings_net_CAbundle.crt",
+					},
+				},
+			},
+			wantError: false,
+		},
+	}
+
+	for _, ts := range tests {
+		tPtr.Run(
+			ts.name, func(t *testing.T) {
+				if _, errorInfo = NewGRPCService(ctv.EXTENSION_HAL, ts.arguments.config); errorInfo.Error != nil {
+					gotError = true
+					errorInfo = errs.ErrorInfo{
+						Error: errors.New(fmt.Sprintf("Failed - gRPC connection was not created for Test: %v", tFunctionName)),
+					}
+				} else {
+					gotError = false
+				}
+				if gotError != ts.wantError {
+					tPtr.Error(ts.name)
+					tPtr.Error(errorInfo)
+				}
+			},
+		)
+	}
+}
