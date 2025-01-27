ee74e39d6ed2cb4e4651693213194717c4e22530
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-01-27 14:57:07 -0800

    Added DK Client extension

--- constantsTypesVars/styh-extensions.go
+++ constantsTypesVars/styh-extensions.go
@@ -5,6 +5,7 @@ const (
 	// Extensions
 	EXTENSION_ADMIN            = "admin"
 	EXTENSION_ANALYZE_QUESTION = "analyze-question"
+	EXTENSION_DK_CLIENT        = "dk-client"
 	EXTENSION_HAL              = "hal"
 	EXTENSION_HTTPS_NATS       = "https-nats"
 	NC_INTERNAL                = "nc-internal"
