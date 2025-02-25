209cdb794d86a3b88344ec01130f5c23d0753087
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-25 09:56:34 -0800

    Split out configurations from extensions

---
+++ constantsTypesVars/styh-configurations.go
@@ -0,0 +1,12 @@
+package sharedServices
+
+//goland:noinspection All
+const (
+	// Extensions
+	CONFIG_GEMINI      = "gemini"
+	CONFIG_GOOGLE_ADS  = "google-ads"
+	CONFIG_GRPC_SERVER = "grpc-server"
+	CONFIG_GRPC_CLIENT = "grpc-client"
+	CONFIG_SENDGRID    = "sendgrid"
+	CONFIG_STRIPE      = "stripe"
+)
--- constantsTypesVars/styh-extensions.go
+++ constantsTypesVars/styh-extensions.go
@@ -3,15 +3,9 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Extensions
-	EXTENSION_ADMIN            = "admin"
-	EXTENSION_ANALYZE_QUESTION = "analyze-question"
-	EXTENSION_DK_CLIENT        = "dk-client"
-	EXTENSION_GOOGLE_ADS       = "google-ads"
-	EXTENSION_HAL              = "hal"
-	EXTENSION_HTTPS_NATS       = "https-nats"
-	NC_INTERNAL                = "nc-internal"
-	EXTENSION_QUICKBOOKS       = "quickbooks"
-	EXTENSION_SAAS_PROFILE     = "saas-profile"
-	EXTENSION_SENDGRID         = "sendgrid"
-	EXTENSION_STRIPE           = "stripe"
+	EXTENSION_ADMIN        = "admin"
+	EXTENSION_DK_CLIENT    = "dk-client"
+	EXTENSION_HAL          = "hal"
+	EXTENSION_HTTPS_NATS   = "https-nats"
+	EXTENSION_SAAS_PROFILE = "saas-profile"
 )
