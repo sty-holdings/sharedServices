57207cc8f490d9f637992a464b09e270f00d4349
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-27 12:03:52 -0800

    Renamed styh-si-topics.go to styh-si-topics-keys and added constants for key names

---
+++ constantsTypesVars/styh-si-topics-keys.go
@@ -0,0 +1,23 @@
+package sharedServices
+
+//goland:noinspection All
+const (
+	// SI = System Instruction
+	SI_TOPIC_AI_QUESTION      = "ai-question"
+	SI_TOPIC_ANALYZE_QUESTION = "analyze-question"
+	SI_TOPIC_DETERMINE_API    = "determine-api"
+	SI_TOPIC_GENERATE_ANSWER  = "generate-answer"
+)
+
+//goland:noinspection All
+const (
+	// SI = System Instruction
+	SI_KEY_SIMPLE_ANSWER                     = "simple-answer"
+	SI_KEY_COMPLEX_ANSWER                    = "complex-answer"
+	SI_KEY_CATEGORY_PROMPY_COMPARISON        = "category-prompt-Comparison"
+	SI_KEY_TIME_PERIOD_SPECIAL_WORDS_PRESENT = "time-period-special-words-present"
+	SI_KEY_TIME_PERIOD_WORDS_PRESENT         = "time-period-words-present"
+	SI_KEY_TIME_PERIOD_VALUES                = "time-period-values"
+	SI_KEY_DETEMINE_API                      = "determine-api"
+	SI_KEY_BUSINESS_ANALYST                  = "business-analyst"
+)
