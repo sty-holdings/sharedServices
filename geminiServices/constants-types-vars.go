4067ff913d8f51fdae35dc3769d28ecf59b8258f
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-27 13:00:52 -0800

    Moved topic and key for Gemini to Gemini Service

--- geminiServices/constants-types-vars.go
+++ geminiServices/constants-types-vars.go
@@ -4,8 +4,27 @@ import (
 	"cloud.google.com/go/vertexai/genai"
 )
 
-//goland:noinspection ALL
-const ()
+//goland:noinspection All
+const (
+	// SI = System Instruction
+	SI_TOPIC_AI_QUESTION      = "ai-question"
+	SI_TOPIC_ANALYZE_QUESTION = "analyze-question"
+	SI_TOPIC_DETERMINE_API    = "determine-api"
+	SI_TOPIC_GENERATE_ANSWER  = "generate-answer"
+)
+
+//goland:noinspection All
+const (
+	// SI = System Instruction
+	SI_KEY_SIMPLE_ANSWER                     = "simple-answer"
+	SI_KEY_COMPLEX_ANSWER                    = "complex-answer"
+	SI_KEY_CATEGORY_PROMPY_COMPARISON        = "category-prompt-Comparison"
+	SI_KEY_TIME_PERIOD_SPECIAL_WORDS_PRESENT = "time-period-special-words-present"
+	SI_KEY_TIME_PERIOD_WORDS_PRESENT         = "time-period-words-present"
+	SI_KEY_TIME_PERIOD_VALUES                = "time-period-values"
+	SI_KEY_DETEMINE_API                      = "determine-api"
+	SI_KEY_BUSINESS_ANALYST                  = "business-analyst"
+)
 
 type GeminiConfig struct {
 	MaxOutputTokens    string             `json:"max_output_tokens"`
