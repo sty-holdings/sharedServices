d7ffa9989b36e6f5ed981188078b4ec833e41121
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-28 15:24:31 -0800

    Moved duplicate categories out of Hal.

--- constantsTypesVars/styh-categories.go
+++ constantsTypesVars/styh-categories.go
@@ -2,13 +2,15 @@ package sharedServices
 
 //goland:noinspection All
 const (
-	SALES_SUB_SALES_STRATEGY           = "sales-strategy"
-	SALES_SUB_SALES_PLANNING           = "sales-planning"
-	SALES_SUB_SALES_OPERATIONS         = "sales-operations"
-	SALES_SUB_SALES_ACQUISITION        = "sakes-acquisition"
-	SALES_SUB_SALES_RETENTION          = "sales-retention"
-	SALES_SUB_SALES_INSIDE             = "sales-inside"
-	SALES_SUB_SALES_ACCOUNT_MANAGEMENT = "sales-account-management"
-	SALES_SUB_SALES_LEAD_GENERATION    = "sales-lead-generation"
-	SALES_SUB_SALES_TRAINING           = "sales-training"
+	CAT_DATE                     = "date"
+	CAT_SALES_STRATEGY           = "sales-strategy"
+	CAT_SALES_PLANNING           = "sales-planning"
+	CAT_SALES_OPERATIONS         = "sales-operations"
+	CAT_SALES_ACQUISITION        = "sales-acquisition"
+	CAT_SALES_RETENTION          = "sales-retention"
+	CAT_SALES_INSIDE             = "sales-inside"
+	CAT_SALES_ACCOUNT_MANAGEMENT = "sales-account_management"
+	CAT_SALES_LEAD_GENERATION    = "sales-lead_generation"
+	CAT_SALES_TRAINING           = "sales-training"
+	CAT_TIME                     = "time"
 )
