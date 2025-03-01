0c5460bf10d99806551a57c786e2a51bca316fd7
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-28 16:43:27 -0800

    Change from rune to string so escape characters work

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,12 +3,10 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = `Hmm, it looks like we couldn't find any information matching your request.
-		\u2022 Has your information been updated in your service provider.
-		\u2022 Try a broader search: Use more general keywords related to the SAAS provider.`
-	CUST_MSG_NEED_DATE_FORMATTED = `The question doesn't have a timeframe'.
-		\u2022 Added a date.
-        \u2022 Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions.
-		\u2022 Added words like current, previous, today, yesterday
-		\u2022 Added last and the timeframe. ex: last week or last month`
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request.\n " +
+		"\u2022 Has your information been updated in your service provider.\n" +
+		"\u2022 Try a broader search: Use more general keywords related to the SAAS provider."
+	CUST_MSG_NEED_DATE_FORMATTED = "The question doesn't have a timeframe.\n" + "\u2022 Added a date.\n" +
+		"\u2022 Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions.\n" +
+		"\u2022 words like current, previous, today, yesterday\n" + "\u2022 last and the timeframe. ex: last week or last month"
 )
