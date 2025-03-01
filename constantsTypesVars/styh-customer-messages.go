d742be9ffd232ab506831329a3fa77a721d07076
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-28 18:19:40 -0800

    Fixed messaging to be clearer

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -4,9 +4,9 @@ package sharedServices
 const (
 	// Categories
 	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request." +
-		"1) Has your information been updated in your service provider." +
-		"2) Try a broader search: Use more general keywords related to the SAAS provider."
+		"1) Has your information been updated in your service provider system. " +
+		"2) Try a broader search: Use more general keywords related to the information you are looking for."
 	CUST_MSG_NEED_DATE_FORMATTED = "The question doesn't have a timeframe." + "1) Added a date." +
 		"2) Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions." +
-		"3) You can use words like current, previous, today, yesterday, last and the time period. ex: last week or last month"
+		"3) You can use words like current, previous, today, yesterday, last and the time period. ex: last year or last month"
 )
