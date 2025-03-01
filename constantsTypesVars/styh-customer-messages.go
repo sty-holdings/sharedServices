b9629bca04e68b7406f884b0d2ff3f3a392540a2
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-02-28 16:47:21 -0800

    Remove newline and repplace bullets with numbers

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,10 +3,10 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request.\n " +
-		"\u2022 Has your information been updated in your service provider.\n" +
-		"\u2022 Try a broader search: Use more general keywords related to the SAAS provider."
-	CUST_MSG_NEED_DATE_FORMATTED = "The question doesn't have a timeframe.\n" + "\u2022 Added a date.\n" +
-		"\u2022 Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions.\n" +
-		"\u2022 words like current, previous, today, yesterday\n" + "\u2022 last and the timeframe. ex: last week or last month"
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request." +
+		"1) Has your information been updated in your service provider." +
+		"2) Try a broader search: Use more general keywords related to the SAAS provider."
+	CUST_MSG_NEED_DATE_FORMATTED = "The question doesn't have a timeframe." + "1) Added a date." +
+		"2) Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions." +
+		"3) words like current, previous, today, yesterday" + "4) last and the timeframe. ex: last week or last month"
 )
