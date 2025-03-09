6dc73add009f7b120f3479876982ead26abd989c
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-09 16:11:01 -0700

    Added SaasProviders tp STYHClient struct

--- clientServices/constants-types-vars.go
+++ clientServices/constants-types-vars.go
@@ -9,15 +9,16 @@ type STYHClient struct {
 	CreateTimestamp struct {
 		Time time.Time `json:"__time__"`
 	} `firebase:"create_timestamp" json:"create_timestamp"`
-	Email        string          `firebase:"email" json:"email"`
-	FirstName    string          `firebase:"first_name" json:"first_name"`
-	LastName     string          `firebase:"last_name" json:"last_name"`
-	MyGoogleAds  GoogleAps       `firebase:"my_google_ads" json:"my_google_ads"`
-	OnBoarded    bool            `firebase:"on_boarded" json:"on_boarded"`
-	SaasProfile  UserSaaSProfile `firebase:"saas_profile" json:"saas_profile"`
-	StyhClientId string          `firebase:"styh_client_id" json:"styh_client_id"`
-	Timezone     string          `firebase:"timezone" json:"timezone"`
-	Uid          string          `firebase:"uid" json:"uid"`
+	Email         string          `firebase:"email" json:"email"`
+	FirstName     string          `firebase:"first_name" json:"first_name"`
+	LastName      string          `firebase:"last_name" json:"last_name"`
+	MyGoogleAds   GoogleAps       `firebase:"my_google_ads" json:"my_google_ads"`
+	OnBoarded     bool            `firebase:"on_boarded" json:"on_boarded"`
+	SaasProfile   UserSaaSProfile `firebase:"saas_profile" json:"saas_profile"`
+	SaasProviders []string        `firestore:"saas_providers,array"`
+	StyhClientId  string          `firebase:"styh_client_id" json:"styh_client_id"`
+	Timezone      string          `firebase:"timezone" json:"timezone"`
+	Uid           string          `firebase:"uid" json:"uid"`
 }
 type GoogleAps struct {
 	AccessToken string `firebase:"access_token" json:"access_token"`
