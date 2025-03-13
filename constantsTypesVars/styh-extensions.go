96c2e7527bddf4b46c367bda202686ed91687828
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-13 09:51:29 -0700

    Added Digits extension

--- constantsTypesVars/styh-extensions.go
+++ constantsTypesVars/styh-extensions.go
@@ -6,6 +6,6 @@ const (
 	EXTENSION_ADMIN        = "admin"
 	EXTENSION_DK_CLIENT    = "dk-client"
 	EXTENSION_HAL          = "hal"
-	EXTENSION_HTTPS_NATS   = "https-nats"
+	EXTENSION_DIGITS       = "digits"
 	EXTENSION_SAAS_PROFILE = "saas-profile"
 )
