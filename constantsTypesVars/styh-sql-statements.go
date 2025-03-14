205a1114a613f06c6392f0be93c07b6c84971d9e
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-14 13:04:40 -0700

    Fixed typo in SELECT_ALL_FROM_TABLE

--- constantsTypesVars/styh-sql-statements.go
+++ constantsTypesVars/styh-sql-statements.go
@@ -6,5 +6,5 @@ const (
 	INSERT_DAILY_PERFORMANCE = "INSERT INTO dkga.daily_performance " +
 		"(campaign_id, campaign_type, campaign_name, date, clicks, impressions, ctr, cpc, spend, cpm, cost_per_conversion, conversion_rate, conversion_value) " +
 		"VALUES (%v);\n"
-	SELECT_ALL_FROM_TABLE = "SELECT * FROM %s.$s;\n"
+	SELECT_ALL_FROM_TABLE = "SELECT * FROM %s.%s;\n"
 )
