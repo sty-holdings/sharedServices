72f3fd964ff40f452fc5724e843c153ae1a77d84
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-14 13:57:45 -0700

    Renamed file and added tables

--- constantsTypesVars/styh-tables-sql-statements.go
+++ constantsTypesVars/styh-tables-sql-statements.go
@@ -2,6 +2,12 @@ package sharedServices
 
 //goland:noinspection All
 const (
+	TBL_DAILY_PERFORMANCE  = "daily_performance"
+	TBL_COUPLER_GOOGLE_ADS = "coupler_google_ads"
+)
+
+//goland:noinspection All
+const (
 	// Text strings
 	INSERT_DAILY_PERFORMANCE = "INSERT INTO dkga.daily_performance " +
 		"(campaign_id, campaign_type, campaign_name, date, clicks, impressions, ctr, cpc, spend, cpm, cost_per_conversion, conversion_rate, conversion_value) " +
