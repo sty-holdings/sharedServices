85a8d595f32d0021c86cd11806fc73f224773aef
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-18 13:58:14 -0700

    Update STYHClient struct and removed UserSaaSProfile & GoogleAps

--- clientServices/constants-types-vars.go
+++ clientServices/constants-types-vars.go
@@ -13,19 +13,9 @@ type STYHClient struct {
 	FirstName     string `firebase:"first_name" json:"first_name"`
 	LastName      string `firebase:"last_name" json:"last_name"`
 	LocationPtr   *time.Location
-	MyGoogleAds   GoogleAps       `firebase:"my_google_ads" json:"my_google_ads"`
-	OnBoarded     bool            `firebase:"on_boarded" json:"on_boarded"`
-	SaasProfile   UserSaaSProfile `firebase:"saas_profile" json:"saas_profile"`
-	SaasProviders []string        `firestore:"saas_providers,array"json:"saas_providers"`
-	StyhClientId  string          `firebase:"styh_client_id" json:"styh_client_id"`
-	Timezone      string          `firebase:"timezone" json:"timezone"`
-	Uid           string          `firebase:"uid" json:"uid"`
-}
-type GoogleAps struct {
-	AccessToken string `firebase:"access_token" json:"access_token"`
-	CustomerId  string `firebase:"customer_id" json:"customer_id"`
-}
-
-type UserSaaSProfile struct {
-	UserSaaSProviders map[string]string
+	OnBoarded     bool     `firebase:"on_boarded" json:"on_boarded"`
+	SaasProviders []string `firestore:"saas_providers,array"json:"saas_providers"`
+	StyhClientId  string   `firebase:"styh_client_id" json:"styh_client_id"`
+	Timezone      string   `firebase:"timezone" json:"timezone"`
+	Uid           string   `firebase:"uid" json:"uid"`
 }
