573905c8c57bde9760dae37895a727e1fa6a0f06
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-19 09:51:30 -0700

    Added Safety Setting to gemini model configuration

--- geminiServices/geminiServices.go
+++ geminiServices/geminiServices.go
@@ -13,6 +13,8 @@ import (
 	"cloud.google.com/go/vertexai/genai"
 	"google.golang.org/api/option"
 
+	"cloud.google.com/go/ai/generativelanguage/apiv1beta/generativelanguagepb"
+
 	ctv "github.com/sty-holdings/sharedServices/v2025/constantsTypesVars"
 	errs "github.com/sty-holdings/sharedServices/v2025/errorServices"
 	hlps "github.com/sty-holdings/sharedServices/v2025/helpers"
@@ -104,6 +106,25 @@ func (geminiServicePtr *GeminiService) buildModelPool() (errorInfo errs.ErrorInf
 		}
 		tFloat32 = float32(tFloat64)
 		geminiServicePtr.modelPtrs[worker].Temperature = &tFloat32
+		tFloat32 = float32(tFloat64)
+		geminiServicePtr.modelPtrs[worker].SafetySettings = []*genai.SafetySetting{
+			{
+				Category:  genai.HarmCategory(generativelanguagepb.HarmCategory_HARM_CATEGORY_HATE_SPEECH),
+				Threshold: genai.HarmBlockThreshold(generativelanguagepb.SafetySetting_BLOCK_LOW_AND_ABOVE),
+			},
+			{
+				Category:  genai.HarmCategory(generativelanguagepb.HarmCategory_HARM_CATEGORY_SEXUALLY_EXPLICIT),
+				Threshold: genai.HarmBlockThreshold(generativelanguagepb.SafetySetting_BLOCK_LOW_AND_ABOVE),
+			},
+			{
+				Category:  genai.HarmCategory(generativelanguagepb.HarmCategory_HARM_CATEGORY_DANGEROUS_CONTENT),
+				Threshold: genai.HarmBlockThreshold(generativelanguagepb.SafetySetting_BLOCK_LOW_AND_ABOVE),
+			},
+			{
+				Category:  genai.HarmCategory(generativelanguagepb.HarmCategory_HARM_CATEGORY_HARASSMENT),
+				Threshold: genai.HarmBlockThreshold(generativelanguagepb.SafetySetting_BLOCK_LOW_AND_ABOVE),
+			},
+		}
 	}
 
 	return
