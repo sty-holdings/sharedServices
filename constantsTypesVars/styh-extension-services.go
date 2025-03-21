549d4eb38911cb3ac3abe2c716102309098c6dfa
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-21 15:30:45 -0700

    Change EXT_SERVICE_GEMINI to EXT_SERVICE_AI

--- constantsTypesVars/styh-extension-services.go
+++ constantsTypesVars/styh-extension-services.go
@@ -4,7 +4,7 @@ package sharedServices
 const (
 	// Extensions
 	EXT_SERVICE_GCP         = "gcp"
-	EXT_SERVICE_GEMINI      = "gemini"
+	EXT_SERVICE_AI          = "ai"
 	EXT_SERVICE_GOOGLE_ADS  = "google-ads"
 	EXT_SERVICE_GRPC_SERVER = "grpc-server"
 	EXT_SERVICE_GRPC_CLIENT = "grpc-client"
