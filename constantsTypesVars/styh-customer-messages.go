523debc040f7659053680106833faed013761e11
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-26 16:01:47 -0700

    Reworked the messages.

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,10 +3,15 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request." +
-		"1) Has your information been updated in your service provider system. " +
-		"2) Try a broader search: Use more general keywords related to the information you are looking for."
-	CUST_MSG_NEED_DATE_FORMATTED = "The question doesn't have a timeframe." + "1) Added a date." +
-		"2) Does the date make sense in the context of the question. Example: A date in the future would not work for Sales questions." +
-		"3) You can use words like current, previous, today, yesterday, last and the time period. ex: last year or last month"
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = `Hmm, it looks like we couldn't find any information matching your request.
+The vendors you have connected to DaveKnows %s %s. If these are inccorect, please enter a help desk ticket at https://www.daveknows.ai/Help.		
+
+Other reasons: 
+	Your question is not related to the vendor listed above. 
+	Your question is too speficic.`
+	//
+	CUST_MSG_NEED_DATE_FORMATTED = `The question doesn't have a timeframe, so I can't help you.
+Please be more specific by adding a date or timeframe to your question.
+
+Example: in 2024, 10/29/2024, last week, last month, last quarter, etc.`
 )
