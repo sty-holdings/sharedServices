7b9590fcfa99897df49bf3d5700e19b29f6f8490
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-26 15:30:15 -0700

    Fixed missing array marker

--- constantsTypesVars/sty-holdings.go
+++ constantsTypesVars/sty-holdings.go
@@ -95,7 +95,7 @@ type AnalyzedQuestion struct {
 
 type CategorySentence struct {
 	Category              []string            `json:"category"`
-	CountBySubject        map[string]int      `json:"count_by_subject"`
+	CountBySubject        []map[string]int    `json:"count_by_subject"`
 	Prompt                string              `json:"prompt"`
 	SentenceSubject       []string            `json:"sentence_subject"`
 	SentenceSubjectAdverb map[string]int      `json:"sentence_subject_adverb"`
