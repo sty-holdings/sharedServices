9e66cb7fe97e94401413c9701aa993ee183c5764
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-26 17:48:06 -0700

    Forgot to replace vendors with %s - Ugh

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,7 +3,7 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request.\nThe vendors you have connected to DaveKnows %s %s. If %s %s incorrect, " +
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request.\nThe %s you have connected to DaveKnows %s %s. If %s %s incorrect, " +
 		"please enter a help desk ticket at https://www.daveknows.ai/Help.\n\nOther reasons:\n\tThere is no data for the question at this time." +
 		"\n\tYour question is not related to the %s listed above.\n\tYour question is too specific."
 	//
