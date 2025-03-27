20f320243c542b3ad7480885b983baa4c16d9b9b
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-03-26 17:28:54 -0700

    Change customer message back to strings

--- constantsTypesVars/styh-customer-messages.go
+++ constantsTypesVars/styh-customer-messages.go
@@ -3,15 +3,9 @@ package sharedServices
 //goland:noinspection All
 const (
 	// Categories
-	CUST_MSG_NO_DATA_FOUND_FORMATTED = `Hmm, it looks like we couldn't find any information matching your request.
-The vendors you have connected to DaveKnows %s %s. If these are inccorect, please enter a help desk ticket at https://www.daveknows.ai/Help.		
-
-Other reasons: 
-	Your question is not related to the vendor listed above. 
-	Your question is too speficic.`
+	CUST_MSG_NO_DATA_FOUND_FORMATTED = "Hmm, it looks like we couldn't find any information matching your request.\nThe vendors you have connected to DaveKnows %s %s. If these are inccorect, " +
+		"please enter a help desk ticket at https://www.daveknows.ai/Help.\n\nOther reasons:\nYour question is not related to the vendor listed above.\nYour question is too speficic."
 	//
-	CUST_MSG_NEED_DATE_FORMATTED = `The question doesn't have a timeframe, so I can't help you.
-Please be more specific by adding a date or timeframe to your question.
-
-Example: in 2024, 10/29/2024, last week, last month, last quarter, etc.`
+	CUST_MSG_NEED_DATE_FORMATTED = "The question doesn't have a timeframe, so I can't help you.\nPlease be more specific by adding a date or timeframe to your question.\n\nExample: in 2024, " +
+		"10/29/2024, last week, last month, last quarter, etc."
 )
