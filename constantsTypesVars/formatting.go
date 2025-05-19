b0330218bdc49c21a628ded7ddb31d7f74410043
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-05-19 12:01:25 -0700

    Add EQUAL_LINE_LEN_5, LINE_LEN_5, and UNDERSCORE_LINE_LEN_5 formats.

--- constantsTypesVars/formatting.go
+++ constantsTypesVars/formatting.go
@@ -8,9 +8,11 @@ const (
 	EQUAL_LINE_LEN_40      = "========================================"
 	EQUAL_LINE_LEN_20      = "===================="
 	EQUAL_LINE_LEN_10      = "=========="
+	EQUAL_LINE_LEN_5       = "====="
 	LINE_LEN_40            = "----------------------------------------"
 	LINE_LEN_20            = "--------------------"
 	LINE_LEN_10            = "----------"
+	LINE_LEN_5             = "-----"
 	SPACES_EIGHT           = "        "
 	SPACES_FOUR            = "    "
 	SPACES_ONE             = " "
@@ -21,4 +23,5 @@ const (
 	UNDERSCORE_LINE_LEN_40 = "________________________________________"
 	UNDERSCORE_LINE_LEN_20 = "____________________"
 	UNDERSCORE_LINE_LEN_10 = "__________"
+	UNDERSCORE_LINE_LEN_5  = "_____"
 )
