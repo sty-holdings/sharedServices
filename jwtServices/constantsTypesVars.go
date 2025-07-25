e5912fa91aad5ac5026e8e1c28e8ff55cf8fdf0d
scott.yacko@sty-holdings.com <scott.yacko@sty-holdings.com>
2025-07-25 13:53:06 -0700

    Moved jwt constants here from Firebase service. Firebase is under protected gcp service.

--- jwtServices/constantsTypesVars.go
+++ jwtServices/constantsTypesVars.go
@@ -33,6 +33,17 @@ const (
 	TOKEN_TYPE_REFRESH = "refresh"
 )
 
+//goland:noinspection ALL
+const (
+	JWT_PAYLOAD_SUBJECT_FN      = "SUBJECT"
+	JWT_PAYLOAD_CLAIMS_FN       = "CLAIMS"
+	JWT_PAYLOAD_AUDIENCE_FN     = "AUDIENCE"
+	JWT_PAYLOAD_REQUESTOR_ID_FN = "REQUESTOR_ID"
+	JWT_PAYLOAD_EXPIRES_FN      = "EXPIRES"
+	JWT_PAYLOAD_ISSUER_FN       = "ISSUER"
+	JWT_PAYLOAD_ISSUED_AT_FN    = "ISSUED_AT"
+)
+
 type GenerateCertificate struct {
 	CertFileName       string
 	Certificate        []byte
